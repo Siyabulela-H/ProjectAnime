e6915de8cd0c4a0f70b6d98e306b523faa92dad0
Siyabulela-H <115037326+Siyabulela-H@users.noreply.github.com>
2023-04-19 15:53:14 +0200

    Add project files.

---
+++ Project1/Controllers/AnimeController1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    public class AnimeController1 : Controller
+    {
+        //This List will store the details of the Animes
+        private static List<Anime> ani=new List<Anime>();
+
+        public IActionResult Index()
+        {
+            return View(ani);
+        }
+        public IActionResult Create()
+        {
+            var animes = new Anime();
+            return View(animes);
+        }
+
+        public IActionResult CreateAnime(Anime anim)
+        {
+            ani.Add(anim);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Project1/Models/Anime.cs
@@ -0,0 +1,8 @@
+﻿namespace Project1.Models
+{
+    public class Anime
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
